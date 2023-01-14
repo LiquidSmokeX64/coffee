f74b55828b0a6194e4be7721b59c9af2d3b67fb1
Dan Maxie <dlc07projectxmods@gmail.com>
2023-01-14 11:38:54 -0800

    16.0.0
    Switched iso to KDE.
    Added installer to desktop

--- releng/profiledef.sh
+++ releng/profiledef.sh
@@ -23,5 +23,6 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/usr/local/bin/coffeebrewer"]="0:0:750"
+  ["/usr/local/bin/coffeebrewer"]="0:0:755"
+  ["/etc/skel/Desktop/coffeebrewer.desktop"]="0:0:755"
 )
