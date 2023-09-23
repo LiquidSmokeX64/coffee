516a50012bce29ac007769da7397efd604858252
Dan Maxie <dlc07projectxmods@gmail.com>
2023-09-23 00:34:10 -0700

    23.0.0
    Added GUI to ISO.
    Added some tweaks to installer script.

--- releng/profiledef.sh
+++ releng/profiledef.sh
@@ -5,7 +5,7 @@ iso_name="coffeelinux"
 iso_label="COFFEE_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Coffee Linux"
 iso_application="Coffee Linux Live/Rescue CD"
-iso_version=22.0.4-final-"$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+iso_version=23.0.0-final-"$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
@@ -17,13 +17,14 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/usr/local/bin/coffeebrewer2"]="0:0:755"
-  
-  
-  
+  ["/usr/share/applications/coffeebrewer.desktop"]="0:0:755"
+  ["/root/Desktop/coffeebrewer.desktop"]="0:0:755"
+  ["/etc/skel/Desktop/coffeebrewer.desktop"]="0:0:755"
 )
