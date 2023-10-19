dabf082af574a7521e3890ff1c6dc229af82f8c3
Dan Maxie <dlc07projectxmods@gmail.com>
2023-10-19 16:52:50 -0700

    23.1.3
    Rebased to a fresh releng profile for archiso

--- releng/profiledef.sh
+++ releng/profiledef.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="coffeelinux"
+iso_name="coffeeiso"
 iso_label="COFFEE_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Coffee Linux"
+iso_publisher="Coffee Linux <https://archlinux.org>"
 iso_application="Coffee Linux Live/Rescue CD"
-iso_version=23.1.2-"$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+iso_version="CoffeeLinux-23.1.3-$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
@@ -17,14 +17,11 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
-  ["/etc/gshadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
+  ["/root/.gnupg"]="0:0:700"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/usr/local/bin/coffeebrewer2"]="0:0:755"
-  ["/usr/share/applications/coffeebrewer.desktop"]="0:0:755"
-  ["/root/Desktop/coffeebrewer.desktop"]="0:0:755"
-  ["/etc/skel/Desktop/coffeebrewer.desktop"]="0:0:755"
 )
