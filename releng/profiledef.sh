f06f294a6bdd30a6ee403408d1f10d7cc6b7eac9
Dan Maxie <dlc07projectxmods@gmail.com>
2023-11-05 22:34:39 -0800

    23.2.3
    Rebased to fresh releng profile due to failure to boot properly.

--- releng/profiledef.sh
+++ releng/profiledef.sh
@@ -17,7 +17,6 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
-  ["/etc/gshadow"]="0:0:400"
   ["/etc/group"]="0:0:400"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
@@ -26,7 +25,4 @@ file_permissions=(
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/usr/local/bin/coffeebrewer2"]="0:0:755"
-  ["/usr/share/applications/coffeebrewer.desktop"]="0:0:755"
-  ["/root/Desktop/coffeebrewer.desktop"]="0:0:755"
-  ["/etc/skel/Desktop/coffeebrewer.desktop"]="0:0:755"
 )
