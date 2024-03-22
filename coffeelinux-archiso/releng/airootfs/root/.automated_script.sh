57be815bc4cfe99ae18b071327c32d3f792c70ce
Dan Maxie <dlc07projectxmods@gmail.com>
2024-03-22 00:23:58 -0700

    25.0.0
    Removed some broken desktop environment choices.
    Removed desktop environment specific installation scripts.
    Renamed scripts accordingly.
    Misc typo fixes.

--- coffeelinux-archiso/releng/airootfs/root/.automated_script.sh
+++ coffeelinux-archiso/releng/airootfs/root/.automated_script.sh
@@ -41,5 +41,5 @@ automated_script() {
 
 if [[ $(tty) == "/dev/tty1" ]]; then
     automated_script
-    coffeebrewer-mainmenu
+    coffee-barista
 fi
--- coffeelinux-archiso/releng/profiledef.sh
+++ coffeelinux-archiso/releng/profiledef.sh
@@ -5,7 +5,7 @@ iso_name="coffeelinux"
 iso_label="COFFEE_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Coffee Linux <https://archlinux.org>"
 iso_application="Coffee Linux Live/Rescue CD"
-iso_version="24.9.2-$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+iso_version="25.0.0-$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
@@ -25,8 +25,6 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/usr/local/bin/coffeebrewer-custom"]="0:0:755"
-  ["/usr/local/bin/coffeebrewer-gnome"]="0:0:755"
-  ["/usr/local/bin/coffeebrewer-kde"]="0:0:755"
-  ["/usr/local/bin/coffeebrewer-mainmenu"]="0:0:755"
+  ["/usr/local/bin/coffeebrewer3"]="0:0:755"
+  ["/usr/local/bin/coffee-barista"]="0:0:755"
 )
