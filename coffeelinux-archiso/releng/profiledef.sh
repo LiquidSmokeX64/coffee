acf636e91a8525f898130ba345acd9d5601d211c
Dan Maxie <dlc07projectxmods@gmail.com>
2025-06-09 20:40:16 -0700

    29.0.3
    Fixed a typo in coffee-keeper.
    Removed coffee-barista replacement from coffee-filter.

--- coffeelinux-archiso/releng/profiledef.sh
+++ coffeelinux-archiso/releng/profiledef.sh
@@ -5,7 +5,7 @@ iso_name="coffeelinux"
 iso_label="COFFEE_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Coffee Linux <https://archlinux.org>"
 iso_application="Coffee Linux Live/Rescue CD"
-iso_version="29.0.2-minimal-installer"
+iso_version="29.0.3-minimal-installer"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
