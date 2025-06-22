a6af1052cd7799294ec2208249345d97eff66e48
Dan Maxie <dlc07projectxmods@gmail.com>
2025-06-22 10:52:21 -0700

    29.6.2
    Added default email and calendar apps to cosmic.
    Fixed a typo.

--- coffeelinux-archiso/releng/profiledef.sh
+++ coffeelinux-archiso/releng/profiledef.sh
@@ -5,7 +5,7 @@ iso_name="coffeelinux"
 iso_label="COFFEE_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Coffee Linux <https://archlinux.org>"
 iso_application="Coffee Linux Live/Rescue CD"
-iso_version="29.6.1-installer"
+iso_version="29.6.2-installer"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
