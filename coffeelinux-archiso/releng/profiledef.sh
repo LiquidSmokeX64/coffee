a5d4f2a5efa07c688fbd2d3a3f84bd94f6d47e6a
Dan Maxie <dlc07projectxmods@gmail.com>
2025-11-19 12:02:45 -0800

    33.5.0
    Added AUR support back in but as an afterthought process.
    This ensures even if it fails, nothing is harmed.

--- coffeelinux-archiso/releng/profiledef.sh
+++ coffeelinux-archiso/releng/profiledef.sh
@@ -5,7 +5,7 @@ iso_name="coffeelinux"
 iso_label="COFFEE_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Coffee Linux <https://archlinux.org>"
 iso_application="Coffee Linux Live/Rescue DVD"
-iso_version="33.4.0"
+iso_version="33.5.0"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux'
