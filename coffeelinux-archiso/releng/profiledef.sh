014b6a8ea4da38ba7597689797e08cd3ca05db15
Dan Maxie <dlc07projectxmods@gmail.com>
2025-12-01 18:19:24 -0800

    34.0.1
    Added fix to allow installing DotNet 4.8 to your Wine Prefixes via Protontricks without failures.
    (Required for Space Engineers)

--- coffeelinux-archiso/releng/profiledef.sh
+++ coffeelinux-archiso/releng/profiledef.sh
@@ -5,7 +5,7 @@ iso_name="coffeelinux"
 iso_label="COFFEE_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Coffee Linux <https://archlinux.org>"
 iso_application="Coffee Linux Live/Rescue DVD"
-iso_version="34.0.0"
+iso_version="34.0.1"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux'
