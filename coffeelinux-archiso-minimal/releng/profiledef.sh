d0b02733eff664fee7cd275418e38a8d90feeda6
Dan Maxie <dlc07projectxmods@gmail.com>
2026-01-01 03:50:28 -0800

    36.3
    Added CoffeeLinux Minimal and LiveIso options to CoffeeLinux Manager's Iso Maker App.
    Fixed a permission setting bug in SteamOS mode.

--- coffeelinux-archiso-minimal/releng/profiledef.sh
+++ coffeelinux-archiso-minimal/releng/profiledef.sh
@@ -5,7 +5,7 @@ iso_name="coffeelinux"
 iso_label="COFFEE_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Coffee Linux <https://archlinux.org>"
 iso_application="Coffee Linux Live/Rescue DVD"
-iso_version="36.3-Harvest"
+iso_version="36.3-Harvest-Minimal"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux'
@@ -16,7 +16,6 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
-  ["/etc/shadow"]="0:0:400"
   ["/etc/gshadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
