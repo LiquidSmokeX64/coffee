1ec074c039ddf01b9f5125badf45531ce8055164
Dan Maxie <dlc07projectxmods@gmail.com>
2026-01-01 03:11:52 -0800

    36.2
    Added more wayland support for virtual keyboards in iso.
    Removed useless applet in LiveIso.

--- coffeelinux-archiso/releng/profiledef.sh
+++ coffeelinux-archiso/releng/profiledef.sh
@@ -5,7 +5,7 @@ iso_name="coffeelinux"
 iso_label="COFFEE_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Coffee Linux <https://archlinux.org>"
 iso_application="Coffee Linux Live/Rescue DVD"
-iso_version="36.1-Harvest"
+iso_version="36.2-Harvest"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux'
