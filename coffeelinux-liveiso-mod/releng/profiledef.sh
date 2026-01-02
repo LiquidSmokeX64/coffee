91908ce3cc567858ccb3a1fb2b9cd765cb0ce39a
Dan Maxie <dlc07projectxmods@gmail.com>
2026-01-01 22:07:38 -0800

    36.6
    Added zram-generator.conf to LiveIso.

--- coffeelinux-liveiso-mod/releng/profiledef.sh
+++ coffeelinux-liveiso-mod/releng/profiledef.sh
@@ -5,7 +5,7 @@ iso_name="coffeelinux"
 iso_label="COFFEE_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Coffee Linux <https://archlinux.org>"
 iso_application="Coffee Linux Live/Rescue DVD"
-iso_version="36.5-Harvest-LiveGUI"
+iso_version="36.6-Harvest-LiveGUI"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux'
