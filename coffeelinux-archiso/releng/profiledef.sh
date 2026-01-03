0e4f2e4a5882030c3bc94e80568697fffbe04c84
Dan Maxie <dlc07projectxmods@gmail.com>
2026-01-03 00:29:24 -0800

    37.0
    Fixed a recurring ui bug in questionnaire segment.
    Added clarity to what choices are selected by default in some areas.

--- coffeelinux-archiso/releng/profiledef.sh
+++ coffeelinux-archiso/releng/profiledef.sh
@@ -5,7 +5,7 @@ iso_name="coffeelinux"
 iso_label="COFFEE_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Coffee Linux <https://archlinux.org>"
 iso_application="Coffee Linux Live/Rescue DVD"
-iso_version="36.8-Harvest-Minimal"
+iso_version="37.0-Infusion-Minimal"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux'
